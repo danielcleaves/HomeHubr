02473e795d1c400dcd0cba1f93ec52d046098361
daniel <daniel_cleaves@hotmail.com>
2017-11-02 09:36:47 -0500

    Adding Files

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-	:publishable_key => 'pk_live_8LVqi5FVXI49EkzQ0phyTjN4',
-	:secret_key => 'sk_live_zrtqeTx9jD2Y44sS9jUqbrsp'
+	# :publishable_key => 'pk_live_8LVqi5FVXI49EkzQ0phyTjN4',
+	# :secret_key => 'sk_live_zrtqeTx9jD2Y44sS9jUqbrsp'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
